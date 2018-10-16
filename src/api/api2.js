7ca4a44d5be3fa66e2d88476cc2e3f38aeb1566f
Chaitanya Giri <girichaitanya11@gmail.com>
2018-10-16 20:15:02 +0530

    Endpoints: /search and /filter configured

--- src/api/api2.js
+++ src/api/api2.js
@@ -1,11 +1,11 @@
 const express    = require('express');
 const app        = express(); 
 const Tweet      = require("./../models/Tweet");
-
+const q2m        = require('query-to-mongo');
 app.use(express.json())
 
-app.post('/',function(req,res){
-	console.log(req.body);
+//   CASE 01 - Only filtering
+app.post('/filter',function(req,res){
   let pageNo = parseInt(req.body.paginate.pageNo)
   let size = parseInt(req.body.paginate.size)
   let paginate = {}
@@ -24,61 +24,51 @@ app.post('/',function(req,res){
 		paginate.limit = 10;
 	}
 	
-  // CASE 01 - Neither filtering nor search parameters provided
-	if(req.body.paginate && !req.body.search && !req.body.filter){
-		if(!req.body.sort){
-			Tweet.find({},{},paginate)
-			.then(function(data) {
-				console.log('Response sent');
-				res.json(data);
-			})
-			.catch(function(err) {
-				console.log('ERROR OCCURED - ' + err);
-				res.json({"error" : false,"message" : "SORRY AN ERROR OCCURRED"});
-			});
-		}else{
-			Tweet.find({},{},paginate).sort(req.body.sort)
-			.then(function(data) {
-				console.log('Response sent');
-				res.json(data);
-			})
-			.catch(function(err) {
-				console.log('ERROR OCCURED - ' + err);
-				res.json({"error" : false,"message" : "SORRY AN ERROR OCCURRED"});
-			});
-		}
-	}
+	let query = q2m(req.query);
+	console.log(query);
+
+	Tweet.find(query.criteria, query.options, paginate)
+	.then(function(data) {
+		console.log('Response sent');
+		res.json(data);
+	})
+	.catch(function(err) {
+		console.log('ERROR OCCURED - ' + err);
+		res.json({"error" : false,"message" : "SORRY AN ERROR OCCURRED"});
+	});
+});
 	
-	// CASE 02 - Only searching parameters provided
-	if(req.body.search && !req.body.filter){
-		if(!req.body.sort){
-			Tweet.find({$text: { $search: req.body.search}})
-				.then(function(data) {
-					console.log('Response sent');
-					res.json({"error" : false, message : data});
-				})
-				.catch(function(err) {
-					console.log('ERROR OCCURED - ' + err);
-					res.json({"error" : false,"message" : "SORRY AN ERROR OCCURRED"});
-				});
-		}else{
-			Tweet.find({$text: { $search: req.body.search}}).sort(req.body.sort)
-				.then(function(data) {
-					console.log('Response sent');
-					res.json({"error" : false, message : data});
-				})
-				.catch(function(err) {
-					console.log('ERROR OCCURED - ' + err);
-					res.json({"error" : false,"message" : "SORRY AN ERROR OCCURRED"});
-				});
-		}
-	}
+app.post('/search',function(req,res){
+	let pageNo = parseInt(req.body.paginate.pageNo)
+  let size = parseInt(req.body.paginate.size)
+  let paginate = {}
+  if(pageNo < 0 || pageNo === 0) {
+        response = {"error" : true,"message" : "invalid page number, should start with 1"};
+        return res.json(response)
+  }
 	
-	// CASE 03 - Only filtering parameters provided 
-	if(!req.body.search && req.body.filter){
-		
+	//Configuring pagination parameters
+	if(req.body.paginate){
+		paginate.skip = size * (pageNo - 1);
+		paginate.limit = size;
+	}else{
+		//Default values for pagination if no pagination parameters provided
+		paginate.skip = 0;
+		paginate.limit = 10;
 	}
+	let query = q2m(req.query);
+	
+	Tweet.find({$text: { $search: req.body.search}},query.options)
+	.then(function(data) {
+		console.log('Response sent');
+		res.json({"error" : false, message : data});
+	})
+	.catch(function(err) {
+		console.log('ERROR OCCURED - ' + err);
+		res.json({"error" : false,"message" : "SORRY AN ERROR OCCURRED"});
+	});
 	
 });
+
 
 module.exports = app;
